71e762d9e4a2a30f31351b14b74de11c92606b45
viniciusd-avila <davila.viniciusbarros@gmail.com>
2018-11-16 02:18:49 -0200

    split DNA

--- bioinfo.py
+++ bioinfo.py
@@ -11,6 +11,8 @@ amn_list = [['Adenine','A'],
  ['Pyrimidines','T','U'],
  ['Non-identified','N']]
 
+codon_list = ['AA','AC','AT']
+
 plot_list = ['Adenine','Cytosine','Guanine','Thymine','Non-identified']
 
 def amino_acids_count(df, amn_list):
@@ -20,6 +22,14 @@ def amino_acids_count(df, amn_list):
             df[amn[0]] = df[amn[0]] + df['Sequence'].apply(lambda seq:seq.count(letter))
         df[amn[0]] = df[amn[0]]/df['Length']
     return df
+
+def split_DNA(seq,n):
+    return [seq[i:i+n] for i in range(0, len(seq), n)]
+
+def codon_count(codon_list,seq,n):
+    sseq = split_DNA(seq,n)
+    return [sum(list(map(lambda x: x.count(codon),sseq))) for codon in codon_list]
+
 
 def gen_df(infile):
     with open(infile, 'r') as myfile:
@@ -31,7 +41,7 @@ def gen_df(infile):
     for i in range(len(data)):
         data[i] = data[i].split('\n',maxsplit=1)
         if len(data[i]) > 1:
-            dic[data[i][0]] = Seq(str(data[i][1].replace('\n','').split()))
+            dic[data[i][0]] = data[i][1].replace('\n','')
     
     df = pd.DataFrame.from_dict(dic,orient='index')
     df.columns = ['Sequence']
@@ -43,7 +53,9 @@ def gen_df(infile):
     df.reset_index(level=0, inplace=True)
     df['Species'] = df['Species'].apply(lambda s: s.replace(' ','-'))
     df['Length'] = df['Sequence'].apply(lambda seq: len(seq))
-    return amino_acids_count(df, amn_list)
+    df = amino_acids_count(df, amn_list)
+    df['Split'] = df['Sequence'].apply(lambda seq: split_DNA(seq,300))
+    return df 
 
 def amino_plot(plot_list):
     return df[plot_list].plot(kind='bar', title ="V comp", figsize=(15, 10), legend=True, stacked=True,fontsize=12)
