a52021867b4728a2257bab65a5e85cfbb027efb5
viniciusd-avila <davila.viniciusbarros@gmail.com>
2018-12-05 10:55:49 -0200

    organizing

--- bioinfo.py
+++ bioinfo.py
@@ -2,158 +2,185 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from Bio.Seq import Seq
+from Bio import AlignIO
+from Bio.Align.Applications import ClustalwCommandline
+from Bio import Phylo
+from sklearn.cluster import DBSCAN
 import itertools
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
 from sklearn.cluster import KMeans
 
-lstamn = ['A','C','G','T']
+aminoacids = ['A','C','G','T']
 
-amn_list = [['Adenine','A'],
- ['Cytosine','C'],
- ['Guanine','G'],
- ['Thymine','T'],
- ['Uracil','U'],
- ['Purine','A','G'],
- ['Pyrimidines','T','U'],
- ['Non-identified','N']]
+codons = list(map((lambda element: ''.join(list(element))), itertools.product(aminoacids,repeat=3)))
 
-plot_list = ['Adenine','Cytosine','Guanine','Thymine','Non-identified']
+def clustalw_alignment(filename):
+    cline = ClustalwCommandline("clustalw2", infile = filename)
+    cline()
+    return AlignIO.read(filename + ".aln","clustal")
 
-def defcodons(n):
-    return list(map((lambda element: ''.join(list(element))), itertools.product(lstamn,repeat=n)))
+def pca_2d(df):
+    X = df.loc[:, codons].values
+    X = StandardScaler().fit_transform(X)
+    y = df.loc[:, codons].values
+    pca = PCA(n_components=2)
+    return pca.fit_transform(X)
 
-def amino_acids_count(df, amn_list):
-    for amn in amn_list:
-        df[amn[0]] = 0.0
-        for letter in amn[1:]:
-            df[amn[0]] = df[amn[0]] + df['Sequence'].apply(lambda seq:seq.count(letter))
-        df[amn[0]] = df[amn[0]]/df['Length']
-    return df
+def t_SNE(df):
+    tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300) 
+    return tsne.fit_transform(df.loc[:,codons].values)
 
-def split_DNA(seq,n):
-    return [seq[i:i+n] for i in range(0, len(seq), n)]
+def kmeans_plot(k,X):
+    kmeans = KMeans(n_clusters=k)
+    kmeans.fit(X)
+    y_means = kmeans.predict(X)
 
-def gen_df2(name,seq,n):
-    df2 = pd.DataFrame(split_DNA(seq,12))
-    df2.columns = ['Fragment']
-    df2 = df2[df2['Fragment'].apply(lambda x: set(x)) != set('N')] #Remove informação de amino ácidos não identificados 
-    for codon in defcodons(n):
-        df2[codon] = df2['Fragment'].apply(lambda frag: float(frag.count(codon)))
-    df2.name = name
-    return df2
+    plt.scatter(X[:,0],X[:,1],c=y_means,s=50,cmap='viridis')
 
-def gen_df(infile):
-    with open(infile, 'r') as myfile:
-        data=myfile.read()
+    centers = kmeans.cluster_centers_
+    plt.scatter(centers[:,0],centers[:,1],c='black',s=200,alpha=0.5)
+
+def dbscan_plot(X):
+    db = DBSCAN(eps=0.3, min_samples=10).fit(X)
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool) 
+    core_samples_mask[db.core_sample_indices_] = True 
+    labels = db.labels_
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+    n_noise_ = list(labels).count(-1)
+    unique_labels = set(labels) 
+    colors = [plt.cm.Spectral(each)           
+        for each in np.linspace(0, 1, len(unique_labels))] 
+    for k, col in zip(unique_labels, colors):     
+        if k == -1:         
+        # Black used for noise.         
+            col = [0, 0, 0, 1]      
+            
+    class_member_mask = (labels == k)      
+    xy = X[class_member_mask & core_samples_mask]     
+    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+                        markeredgecolor='k', markersize=14)      
+    xy = X[class_member_mask & ~core_samples_mask]     
+    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+                    markeredgecolor='k', markersize=6)  
     
+    plt.title('Estimated number of clusters: %d' % n_clusters_) 
+    plt.show()
+
+def clean_data(infile):
+    with open(infile,'r') as myfile:
+        data=myfile.read()
+
     data = data.split('>')
     dic = {}
 
     for i in range(len(data)):
         data[i] = data[i].split('\n',maxsplit=1)
-        if len(data[i]) > 1:
+        if len(data[i])>1:
             dic[data[i][0]] = data[i][1].replace('\n','')
+    return dic
+
+def gen_species(dic,dim_red=False):
+    species_list = []  
+    for key in dic:     
+        x = Species(key.split(' ')[1] + ' ' + key.split(' ')[2], key.split(' ')[0],dic[key],dim_red)     
+        species_list.append(x)
+    return species_list
+
+def species_name_list(species_list):
+    res = []
+    for species in species_list:
+        res.append(species.name)
+    return res
+
+def tree_nodes_names(dnd_file,species_list):
+    dic = {}
+    for species in species_list:
+        dic[species.accession_number] = species.name.replace(' ','-')
     
-    df = pd.DataFrame.from_dict(dic,orient='index')
-    df.columns = ['Sequence']
-    df = df.reset_index()
-    df['Accession number'] = df['index'].apply(lambda name: name.split(' ')[0])
-    df['Species'] = df['index'].apply(lambda name: name.split(' ')[1] + ' ' + name.split(' ')[2])
-    df = df[['Accession number','Species','Sequence']]
-    df = df.set_index('Species')
-    df.reset_index(level=0, inplace=True)
-    df['Species'] = df['Species'].apply(lambda s: s.replace(' ','-'))
-    df['Length'] = df['Sequence'].apply(lambda seq: len(seq))
-    df = amino_acids_count(df, amn_list)
-    return df 
-
-def amino_plot(plot_list):
-    return df[plot_list].plot(kind='bar', title ="V comp", figsize=(15, 10), legend=True, stacked=True,fontsize=12)
-
-def tree_node_names(dnd_file, df):
-    with open(dnd_file, 'r') as myfile:
+    with open(dnd_file,'r') as myfile:
         text=myfile.read()
-    rep = df.set_index('Accession number').to_dict()['Species']
-    for i, j in rep.items():
-        text = text.replace(i, j)
+
+    for i,j in dic.items():
+        text = text.replace(i,j)
     return text
 
-def pca(df):
-    x = df.loc[:, df.columns != 'Fragment'].values
-    y = df.loc[:, df.columns == 'Fragment'].values
-    x = StandardScaler().fit_transform(x)
-    
-    pca = PCA(n_components=2)
-    
-    principalComponents = pca.fit_transform(x)
-    
-    principalDf = pd.DataFrame(data = principalComponents, 
-            columns = ['principal component 1', 'principal component 2'])
-    
-    finaldDf = pd.concat([principalDf, df[['Fragment']]], axis = 1)
-    finaldDf.name = df.name
-    return finaldDf
 
-def plot_pca(finalDf):
-    fig = plt.figure(figsize = (8,8))
-    ax = fig.add_subplot(1,1,1)
-    ax.set_xlabel('Principal Component 1', fontsize = 15)
-    ax.set_ylabel('Principal Component 2', fontsize = 15)
-    ax.set_title(finalDf.name, fontsize = 20)
-    
-    ax.scatter(finalDf.loc[:, 'principal component 1'], finalDf.loc[:, 'principal component 2'])
-    
-    ax.grid()
+def attr_alignment(species_list,alignment):
+    for species in species_list:
+        for recs in alignment:
+            if species.accession_number == recs.id:
+                species.aligned_seq = str(recs.seq)
 
 def lavenshtein_dist(stringA,stringB):
     m = len(stringA)
     n = len(stringB)
-    
     d = np.zeros(shape=(m,n))
-
     d[0,:] = np.arange(1,n+1)
     d[:,0] = np.arange(1,m+1)
-    
     for j in range(1,n):
         for i in range(1,m):
             if stringA[i] == stringB[j]:
-                d[i, j] = d[i-1, j-1]    
+                d[i, j] = d[i-1, j-1]
             else:
-                d[i,j] = min(d[i-1,j] + 1,
-                                d[i,j-1],
-                                d[i-1,j-1] + 1)
-                
+                d[i,j] = min(d[i-1,j] + 1,d[i,j-1],d[i-1,j-1] + 1)
     return d[m-1,n-1] - 1
 
-from sklearn.cluster import KMeans
+def laven_dist_species(species_list,alignment):
+    attr_alignment(species_list,alignment)
+    length = len(species_list)
 
-def kmeans_plot(X,n):
-    kmeans = KMeans(n_clusters=n)
-    kmeans.fit(X)
-    y_kmeans = kmeans.predict(X)
-
-    plt.scatter(X[:, 0], X[:, 1], c=y_kmeans, s=50, cmap='viridis')
-
-    centers = kmeans.cluster_centers_
-    plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5);
-    
-
-###############################################################################################################################
-#    A função abaixo é o primeiro passo da implementação do Neighbor Joining, como importamos o método a função deixou de ser #
-# utilizada no código. 
-    
-def Q_matrix(matrix): 
-    Q_matrix = matrix
-    for i in range(len(matrix)):
-        
-        for j in range(len(matrix)):
+    dm = np.empty(shape=(length,length))
+    dm[:] = np.nan
+    for i in range(length):
+        for j in range(length):
             if i == j:
-                Q_matrix[i,j]== 0
-            else:
-                Q_matriz[i,j] = (len(matriz) - 2)*matrix[i,j] - np.sum(matrix[:,i]) - np.sum(matrix[:,j])
-                    
-    return Q_matriz
+                dm[i,j] = 0.0
+            elif np.isnan(dm[i,j]):
+                dm[i,j] = dm[j,i] = lavenshtein_dist(species_list[i].aligned_seq,species_list[j].aligned_seq) 
+    pd.DataFrame(dm).to_csv("lavenshtein_distance_species.csv")
+    return dm
 
+def codon_freq_multi_species(species_list):
+    length = len(species_list)
+    
+    df = pd.DataFrame()
+    
+    label = 0
+    for species in species_list:
+        df_tmp = species.codon_freq_df.copy()
+        y = np.empty(len(df_tmp.index))
+        y[:] = label
+        df_tmp['label'] = y
+        df_tmp.reset_index(drop=True,inplace=True)
+        df.reset_index(drop=True,inplace=True)
+        df = pd.concat([df,df_tmp],axis=0)
+        label = label + 1
+    df.reset_index(drop=True,inplace=True)
+    return df
+        
 
+class Species():
+    def __init__(self,name,accession_number,sequence,red_dimension=False):
+        self.name = name
+        self.accession_number = accession_number
+        self.sequence = sequence
+        self.split_seq = self.split_sequence(24)
+        self.codon_freq_df = self.codon_freq()
+        if red_dimension:
+            self.principal_components = pca_2d(self.codon_freq_df)
+            self.tsne_results = t_SNE(self.codon_freq_df)
+
+    def split_sequence(self,n):
+        res = [self.sequence[i:i+n] for i in range(0,len(self.sequence),n)]
+        #res = list(filter(lambda x: set(x) != set('N'),res))
+        return res
+
+    def codon_freq(self):
+        df = pd.DataFrame()
+        df['Fragment'] = self.split_seq
+        for cdn in codons:
+            df[cdn] = df['Fragment'].apply(lambda frgmnt: frgmnt.count(cdn))
+        return df
 
